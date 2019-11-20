22a9b64444c4dc8fadd763159f400c949d8eb93d
aispatr1 <aispatr1@in.ibm.com>
2019-11-20 09:59:13 +0530

    my commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Note} from './note';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,51 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   errMessage: string;
+  note:Note=new Note();
+  notes:Array<Note>= [];
 
+
+constructor(private notesService: NotesService)
+ {
+ }
+ ngOnInit()
+ {
+   this.notesService.getNotes().subscribe(
+     data=>{
+       console.log(data);
+       this.notes = data;
+     },
+     error=>
+     {
+       // console.log(error);
+       this.errMessage=error.message;
+     }
+   )
+ }
+ takeNote()
+ {
+   // console.log(this.note);
+   if((this.note.title ||this.note.text)!='')
+   {
+   this.notesService.addNote(this.note).subscribe(
+     data=>{
+       
+           this.notes.push(data);
+       
+     },
+     error=>
+     {
+      //  console.log(error);
+       this.errMessage=error.message;
+     }
+   )
+   this.note = new Note();
+ }
+
+else {
+  this.errMessage='Title and Text both are required fields'
+    console.log(this.errMessage);
+}
+ 
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component'
+import { HttpClientModule} from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NotesService } from './notes.service';
+import {MatButtonModule} from '@angular/material/button'
 
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [ AppComponent,HeaderComponent],
+  imports: [BrowserModule,HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule ,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule],
+  providers: [NotesService],
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
